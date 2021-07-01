da244ca9baec75fcdab6af4bc87a0faf97df5477
Viviane <oviviane772@gmail.com>
2021-07-01 10:02:39 -0300

    Metodo HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,36 @@
+const { request, response } = require('express');
 const express = require('express');
 
 const app = express();
+app.listen(3333);
 
 app.get('/projects', (request, response) => {
 
-  return response.json({ message: 'Hello World!' });
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+})
+app.post('/projects',(request, response) =>{
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
+app.put('/projects/:id',(request, response) =>{
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ]) 
 })
 
-app.listen(3333);
+app.delete('/projects/:id',(request, response) =>{
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ])
+})
+
+
